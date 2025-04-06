5cede12410ee8666173aee9fc959959669a91ac8
BVasilev727 <frilans569@gmail.com>
2025-04-06 18:43:05 +0300

    Added CORS

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const {errorHandler} = require('./middleware/errorMiddleware');
 const connectDB = require('./connect/database')
 const port = process.env.PORT || 5000;
 const app = express();
+const Cors = require('cors')
 
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(Cors())
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
